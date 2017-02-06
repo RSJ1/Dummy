30c4b1b9e095b7c9bb944b1b57bb1436adafc04e
RSJ <hy3da@naver.com>
2017-02-06 14:17:33 +0900

    remote repository add a HelloGitHub.java

---
+++ HelloGitHub/src/com/sist/HelloGitHub.java
@@ -0,0 +1,13 @@
+package com.sist;
+
+public class HelloGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Hello GIt");
+		System.out.println("Hello GIt2");
+		System.out.println("Hello GIt3");
+		System.out.println("Hello GIt3-1");
+		System.out.println("conflict");
+	}
+
+}
